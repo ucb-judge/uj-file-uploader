5b023eccfee86093aaa485fa7b57925f0c0bf055
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-05-07 13:59:29 -0400

    initial commit

---
+++ src/main/kotlin/ucb/judge/ujfileuploader/UjFileUploaderApplication.kt
@@ -0,0 +1,11 @@
+package ucb.judge.ujfileuploader
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UjFileUploaderApplication
+
+fun main(args: Array<String>) {
+	runApplication<UjFileUploaderApplication>(*args)
+}
---
+++ src/test/kotlin/ucb/judge/ujfileuploader/UjFileUploaderApplicationTests.kt
@@ -0,0 +1,13 @@
+package ucb.judge.ujfileuploader
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UjFileUploaderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
